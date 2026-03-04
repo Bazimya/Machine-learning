913d7d6d39bad33bb9a04d20e5de944f170562d2
Bazimya <sharifbazimya@icloud.com>
2026-03-04 09:31:25 +0200

    Added requirements file

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 import joblib
 import numpy as np
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+import os
+
+# Serve static files from 'static' folder
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Serve index.html at root
+@app.get("/")
+def serve_frontend():
+    return FileResponse(os.path.join("static", "index.html"))
 
 app = FastAPI()
 app.add_middleware(
